834c0a309023e08021abaff8887b3c676faa044b
Graham <ham123.43+github@gmail.com>
2014-01-01 22:21:35 +0000

    Album code cleanup

--- album.php
+++ album.php
@@ -7,36 +7,31 @@ include_once 'inc/gallery.inc.php';
 $albumArray = listDir($DIR);
 $album = $_GET['album'];
 if (!in_array($album, $albumArray)) {
-    echo '<div class="container">
+?>
+<div class="container">
 <h1 class="1of1 center">Album not valid.</h1>
 <p class="1of1 center"><a href="portfolio.php">Go back to the portfolio page</a></p>
-</div>';
+</div>
+<?php
     include_once 'inc/footer.php';
     die();
 }
 
-// helper functions for generating prev and next links
+// functions for generating prev and next links
 function albumLink($nextOrPrev) {
     global $album;
     global $albumArray;
     $key = array_search($album, $albumArray);
 
     if ($nextOrPrev == 'next') {
-        if ($key != (count($albumArray) - 1)) {
-            $key++;
-        } else {
-            $key = 0;
-        }
+        $key = ($key + 1) % count($albumArray);
     } else if ($nextOrPrev == 'prev') {
-        if ($key != 0) {
-            $key--;
-        } else {
-            $key = count($albumArray) - 1;
-        }
+        $count = count($albumArray);
+        $key = ($key + $count - 1) % $count;
     }
 
     $linkAlbumName = $albumArray[$key];
-    return '<a href="album.php?album=' . $linkAlbumName . '" style="display:block;">' . $linkAlbumName . '</a>';
+    return '<a href="album.php?album=' . $linkAlbumName . '" style="display:block">' . $linkAlbumName . '</a>';
         
 }
 function prevLink() {
@@ -47,11 +42,11 @@ function nextLink() {
 }
 ?>
         <div id="album" class="container">
+        <div class="grid 1of4 center remove-padding prev-link"><?php echo prevLink(); ?></div>
+        <div class="grid 2of4 center remove-padding album-title"><?php echo $album; ?></div>
+        <div class="grid 1of4 center remove-padding next-link"><?php echo nextLink(); ?></div>
+        <div class="grid 1of1 remove-padding center">...</div>
 <?php
-echo '<div class="grid 1of4 center remove-padding prev-link">', prevLink(), '</div>';
-echo '<div class="grid 2of4 center remove-padding album-title">', $album, '</div>';
-echo '<div class="grid 1of4 center remove-padding next-link">', nextLink(), '</div>
-<div class="grid 1of1"></div>';
 // traverse album dir
 $imageArray = listDir(makePath($DIR, $album));
 foreach ($imageArray as $imageName) {
@@ -60,15 +55,18 @@ foreach ($imageArray as $imageName) {
         continue;
     }
 	checkAndCreateThumbnail($album, $imageName);
-    echo '
-        <div class="album-image grid 1of4">
-          <a href="', imagePath($album, $imageName), '" data-lightbox="', $album, '" title="', stripFileExt($imageName), '">
-            <img src="', thumbPath($album, $imageName), '" width="', $thumbSize, '" height="', $thumbSize, '" alt="', stripFileExt($imageName), '"/>
-          </a>
-        </div>', "\n";
-}
-echo '        </div>
-        <script src="js/jquery-1.10.2.min.js"></script>
-        <script src="js/lightbox-2.6.min.js"></script>';
-include_once 'inc/footer.php';
 ?>
+
+        <div class="album-image grid 1of4">
+            <a href="<?php echo imagePath($album, $imageName); ?>" data-lightbox="<?php echo $album; ?>" title="<?php echo stripFileExt($imageName); ?>">
+                <img src="<?php echo thumbPath($album, $imageName); ?>" width="<?php echo $thumbSize; ?>" height="<?php echo $thumbSize; ?>" alt="<?php echo stripFileExt($imageName); ?>"/>
+            </a>
+        </div>
+
+<?php } ?>
+        </div>
+        <script src="js/jquery-1.10.2.min.js"></script>
+        <script src="js/lightbox-2.6.min.js"></script>
+
+<?php include_once 'inc/footer.php'; ?>
+
