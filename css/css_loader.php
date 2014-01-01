90e1a8baf847f323bbc4625b6039dd8f723add77
Graham <ham123.43+github@gmail.com>
2014-01-01 22:24:09 +0000

    Added Etag, date modified, 304 header and tidy up to css_loader

--- css/css_loader.php
+++ css/css_loader.php
@@ -1,8 +1,39 @@
 <?php
-header('Content-type: text/css');
+/* The CSS files to combine */
+$cssFiles = array('melody.css', 'tpp.css', 'lightbox.css');
+
+/* Calculates the last modified and md5 of the above files, including this php file */
+$lastModified = filemtime(__FILE__);
+$etagHash = md5_file(__FILE__);
+foreach ($cssFiles as $file) {
+    $etagHash .= md5_file($file);
+    $mod = filemtime($file);
+    if ($mod > $lastModified) {
+        $lastModified = $mod;
+    }
+}
+$etagHash = md5($etagHash);
+
+/* Processes headers from the client */
+$ifModifiedSince = (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? @strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) : false);
+$etagHeader = (isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH']) : false);
+
+/* Sends headers back to the client */
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $lastModified) . ' GMT'); //set last-modified header
+header('Etag: ' . $etagHash);    //set etag-header
+
+
 $offset = 60 * 60 * 24 * 7; // Cache for a week
 header ('Cache-Control: max-age=' . $offset . ', must-revalidate');
 header ('Expires: ' . gmdate ('D, d M Y H:i:s', time() + $offset) . ' GMT');
+header('Content-type: text/css');
+
+//check if page has changed. If not, send 304 and exit
+if ($ifModifiedSince == $lastModified || $etagHeader == $etagHash) {
+    header('HTTP/1.1 304 Not Modified');
+    exit;
+}
+
 ob_start('compress');
 function compress($buffer) {
     /* remove comments */
@@ -11,8 +42,8 @@ function compress($buffer) {
     $buffer = str_replace(array("\r\n\r\n", "\r\r", "\n\n", "\t", '  ', '    ', '    '), '', $buffer);
     return $buffer;
 }
-include('melody.css');
-include('tpp.css');
-include('lightbox.css');
+foreach($cssFiles as $file) {
+    include($file);
+}
 ob_end_flush();
 ?>
