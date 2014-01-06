4706bdbf3cf27d2f787afaea9580dc6b8f001a30
Graham <ham123.43+github@gmail.com>
2014-01-06 00:22:46 +0000

    updated about styles and added underline to links, also included the portfolio and thumbs folders

--- about.php
+++ about.php
@@ -3,13 +3,16 @@
     include_once 'inc/header.php';
 ?>
         <div class="container">
-        <div class="1of1">
+        <div class="grid 1of1 center" >
 			<h1><?php echo $fullCompanyName; ?></h1>
         </div>
-		<div class="grid 1of1">
+		<div class="grid 1of1 center remove-padding about">
+<?php
+if (file_exists('inc/about.html')) {
+    include 'inc/about.html';
+} else { ?>
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque leo elit, egestas dictum pharetra vitae, feugiat viverra odio. Vestibulum et ultrices est, placerat sagittis nisl. Morbi et eleifend ipsum. Ut consectetur viverra velit, sed cursus tellus viverra eu. Fusce massa neque, ullamcorper ac risus et, iaculis rhoncus nulla. Curabitur ac tempus mi. Vivamus aliquet leo nisi. Sed sagittis, sapien et rhoncus luctus, augue tellus posuere libero, ut faucibus urna metus ac erat.
-
-            <p>Aliquam erat volutpat. Etiam aliquet iaculis ante. Donec rutrum cursus rhoncus. Nulla justo mauris, volutpat eget nisi non, viverra aliquam mi. Pellentesque commodo, mi sit amet viverra euismod, ante turpis posuere nunc, eu molestie tellus sapien eget urna. Vestibulum enim neque, pellentesque in consequat et, dictum ut lorem. Proin accumsan, enim sit amet pulvinar mattis, nisi nisl luctus elit, blandit consequat neque tortor vitae risus. Aliquam eget est eget massa convallis commodo. Nullam in dignissim magna. Cras ac lectus neque.
+<?php } ?>
 		</div>
         </div>
 <?php include_once 'inc/footer.php' ?>
