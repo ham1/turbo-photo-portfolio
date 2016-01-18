60603509445b2bc6eead31d91cc783efdecb255e
Graham Russell <graham@ham1.co.uk>
2016-01-18 18:10:19 +0000

    Upgraded lightbox to v2.8.2, compressed pngs.

--- album.php
+++ album.php
@@ -8,65 +8,67 @@ $albumArray = listDir($DIR);
 $album = $_GET['album'];
 if (!in_array($album, $albumArray)) {
 ?>
-<div class="container">
-<h1 class="1of1 center">Album not valid.</h1>
-<p class="1of1 center"><a href="portfolio.php">Go back to the portfolio page</a></p>
-</div>
-<?php
-    include_once 'inc/footer.php';
-    die();
+  <div class="container">
+  <h1 class="1of1 center">Album not valid.</h1>
+  <p class="1of1 center"><a href="portfolio.php">Go back to the portfolio page</a></p>
+  </div>
+  <?php
+  include_once 'inc/footer.php';
+  die();
 }
 
 // functions for generating prev and next links
 function albumLink($nextOrPrev) {
-    global $album;
-    global $albumArray;
-    $key = array_search($album, $albumArray);
-    $count = count($albumArray);
+  global $album;
+  global $albumArray;
+  $key = array_search($album, $albumArray);
+  $count = count($albumArray);
 
-    if ($nextOrPrev == 'next') {
-        $key = ($key + 1) % $count;
-    } else if ($nextOrPrev == 'prev') {
-        $key = ($key + $count - 1) % $count;
-    }
+  if ($nextOrPrev == 'next') {
+    $key = ($key + 1) % $count;
+  } else if ($nextOrPrev == 'prev') {
+    $key = ($key + $count - 1) % $count;
+  }
 
-    $linkAlbumName = $albumArray[$key];
-    return '<a href="album.php?album=' . $linkAlbumName . '" style="display:block">' . prettyPrintFileName($linkAlbumName) . '</a>';
-        
+  $linkAlbumName = $albumArray[$key];
+  return '<a href="album.php?album='
+          . $linkAlbumName
+          . '" style="display:block">'
+          . prettyPrintFileName($linkAlbumName)
+          . '</a>';
+    
 }
 function prevLink() {
-    return albumLink('prev');
+  return albumLink('prev');
 }
 function nextLink() {
-    return albumLink('next');
+  return albumLink('next');
 }
 ?>
-        <div id="album" class="container">
-        <div class="grid 1of4 center remove-padding prev-link"><?php echo prevLink(); ?></div>
-        <div class="grid 2of4 center remove-padding album-title"><?php echo prettyPrintFileName($album); ?></div>
-        <div class="grid 1of4 center remove-padding next-link"><?php echo nextLink(); ?></div>
-        <div class="grid 1of1 remove-padding center">...</div>
+    <div id="album" class="container">
+    <div class="grid 1of4 center remove-padding prev-link"><?php echo prevLink(); ?></div>
+    <div class="grid 2of4 center remove-padding album-title"><?php echo prettyPrintFileName($album); ?></div>
+    <div class="grid 1of4 center remove-padding next-link"><?php echo nextLink(); ?></div>
+    <div class="grid 1of1 remove-padding center">...</div>
 <?php
 // traverse album dir
 $imageArray = listDir(makePath($DIR, $album));
 foreach ($imageArray as $imageName) {
-    // skip non-jpg files
-    if (!isJpg($imageName)) {
-        continue;
-    }
-	checkAndCreateThumbnail($album, $imageName);
+  // skip non-jpg files
+  if (!isJpg($imageName)) {
+    continue;
+  }
+  checkAndCreateThumbnail($album, $imageName);
 ?>
 
-        <div class="album-image grid 1of4">
-            <a href="<?php echo imagePath($album, $imageName); ?>" data-lightbox="<?php echo $album; ?>" title="<?php echo prettyPrintFileName($imageName); ?>">
-                <img src="<?php echo thumbPath($album, $imageName); ?>" width="<?php echo $thumbSize; ?>" height="<?php echo $thumbSize; ?>" alt="<?php echo prettyPrintFileName($imageName); ?>"/>
-            </a>
-        </div>
+    <div class="album-image grid 1of4">
+      <a href="<?php echo imagePath($album, $imageName); ?>" data-lightbox="<?php echo $album; ?>" title="<?php echo prettyPrintFileName($imageName); ?>">
+        <img src="<?php echo thumbPath($album, $imageName); ?>" width="<?php echo $thumbSize; ?>" height="<?php echo $thumbSize; ?>" alt="<?php echo prettyPrintFileName($imageName); ?>"/>
+      </a>
+    </div>
 
 <?php } ?>
-        </div>
-        <script src="js/jquery-1.11.0.min.js"></script>
-        <script src="js/lightbox.min.js"></script>
+    </div>
+    <script src="js/lightbox-plus-jquery.min.js"></script>
 
 <?php include_once 'inc/footer.php'; ?>
-
--- css/css_loader.php
+++ css/css_loader.php
@@ -1,6 +1,8 @@
 <?php
-/* The CSS files to combine */
-$cssFiles = array('melody.css', 'tpp.css', 'lightbox.css');
+// The CSS files to combine
+$cssFiles = array('melody.css', 'tpp.css', 'lightbox.min.css');
+
+/* Deal with HTTP headers */
 
 /* Calculates the last modified and md5 of the above files, including this php file */
 $lastModified = filemtime(__FILE__);
@@ -19,26 +21,25 @@ $ifModifiedSince = (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? @strtotime($_SER
 $etagHeader = (isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH']) : false);
 
 /* Sends headers back to the client */
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $lastModified) . ' GMT'); //set last-modified header
-header('Etag: ' . $etagHash);    //set etag-header
-
-
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $lastModified) . ' GMT');
+header('Etag: ' . $etagHash);
 $offset = 60 * 60 * 24 * 7; // Cache for a week
-header ('Cache-Control: max-age=' . $offset . ', must-revalidate');
-header ('Expires: ' . gmdate ('D, d M Y H:i:s', time() + $offset) . ' GMT');
+header('Cache-Control: max-age=' . $offset . ', must-revalidate');
+header('Expires: ' . gmdate ('D, d M Y H:i:s', time() + $offset) . ' GMT');
 header('Content-type: text/css');
 
-//check if page has changed. If not, send 304 and exit
+// check if page has changed. If not, send 304 and exit
 if ($ifModifiedSince == $lastModified || $etagHeader == $etagHash) {
     header('HTTP/1.1 304 Not Modified');
     exit;
 }
 
+/* send the content */
 ob_start('compress');
 function compress($buffer) {
     /* remove comments */
     $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
-    /* remove double newlines and all tabs, spaces */
+    /* remove double newlines and spaces and all tabs */
     $buffer = str_replace(array("\r\n\r\n", "\r\r", "\n\n", "\t", '  ', '    ', '    '), '', $buffer);
     return $buffer;
 }
