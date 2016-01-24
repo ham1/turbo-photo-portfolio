66ab9e1c876ff480097fc6f279bdb25e6df1fc74
Graham Russell <graham@ham1.co.uk>
2016-01-24 01:32:48 +0000

    Fixed invalid ETag header and other minor tweaks

--- css/css_loader.php
+++ css/css_loader.php
@@ -6,39 +6,40 @@ $cssFiles = array('melody.css', 'tpp.css', 'lightbox.min.css');
 
 // Calculates last modified and md5 of the above files, including this php file
 $lastModified = filemtime(__FILE__);
-$etagHash = md5_file(__FILE__);
+$eTagHash = md5_file(__FILE__);
 foreach ($cssFiles as $file) {
-    $etagHash .= md5_file($file);
+    $eTagHash .= md5_file($file);
     $mod = filemtime($file);
     if ($mod > $lastModified) {
         $lastModified = $mod;
     }
 }
-$etagHash = md5($etagHash);
+$eTagHash = md5($eTagHash);
 
 // Processes headers from the client
 $ifModifiedSince =
     (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])
         ? @strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])
         : false);
-$etagHeader =
+$eTagHeader =
     (isset($_SERVER['HTTP_IF_NONE_MATCH'])
         ? trim($_SERVER['HTTP_IF_NONE_MATCH'])
         : false);
 
 // Sends headers back to the client
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $lastModified) . ' GMT');
-header('Etag: ' . $etagHash);
+header('Etag: "' . $eTagHash . '"');
 $secondsPerWeek = 60 * 60 * 24 * 7;
 header('Cache-Control: max-age=' . $secondsPerWeek . ', must-revalidate');
 header('Expires: ' . gmdate ('D, d M Y H:i:s', time() + $secondsPerWeek) . ' GMT');
-header('Content-type: text/css');
 
 // check if page has changed. If not, send 304 and exit
-if ($ifModifiedSince == $lastModified || $etagHeader == $etagHash) {
+if ($ifModifiedSince > $lastModified || $eTagHeader == $eTagHash) {
     header('HTTP/1.1 304 Not Modified');
     exit();
 }
+// Content-type needed for the body, not required if we send the 304 above.
+header('Content-type: text/css');
 
 /* send the content */
 
